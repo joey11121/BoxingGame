f4de49f70730809873d77277a2b175b4a79cee81
Liang Yu Zhen <joey60209@gmail.com>
2023-01-13 17:31:38 +0800

    Version 2: Revise to Role.h & Role.cpp

--- BoxingGame/Role.cpp
+++ BoxingGame/Role.cpp
@@ -1,37 +1,114 @@
 #include "Role.h"
+#include "GameWindow.h"
 
-Role::Role(int _hp, int _atk, int _def, string _name) {
+Role::Role(int _hp, int _atk, int _def, int _dir, string _name) {
 	HealthPoint = _hp;
-	Defence = _def;
 	AttackPower = _atk;
+	Defense = _def;
 	Energy = 0;
+	dir = _dir; //player 1的角色朝右(=1)，player2的角色朝左(=0)
+	rolestatus = 1; 
 	Name = _name;
 }
-void Role::Load_Attack() {
 
-	return;
+int Role::getHP() {
+	return HealthPoint;
 }
-void Role::Load_Move() {
-	return;
+int Role::getATK() {
+	return AttackPower;
 }
-void Role::Load_Ult() {
-	return;
+int Role:: getEnergy() {
+	return Energy;
 }
+int Role::getDefense() {
+	return Defense;
+}
+
 
 //HP Control
-bool Role::substract_HP(int harm_point) {
-	HealthPoint -= (harm_point - Defence);
+bool Role::substractHP(int Harm_point) {
+	HealthPoint -= Harm_point;
+	if (rolestatus == 0) {
+		HealthPoint -= (Harm_point - Defense);
+	}
+	else {
+		HealthPoint -= Harm_point;
+	}
 	return (HealthPoint <= 0);
 }
 
-//Energy Control
+//Anger Control, Once the role hit the enemy, add 10; once the anger is 100, release ultimate ability. 
 bool Role::increase_Energy() {
-	Energy += 10;
-	if (Energy == MAX_ENERGY)
-		return true;
-	return false;
+	Energy += 5;
+	return (Energy == MAX_ENERGY);
 }
 void Role::reset_Energy() {
 	Energy = 0;
-	return;
+};
+
+//movement of the role, 橫向move兩者會一起改變, 縱向move依據跳、蹲有不同的結果
+void Role::move_right() {
+	if (dir == 0) dir = 1;
+	if (x2 + step > win_width) {
+		x1 = win_width - (x2 - x1);
+		x2 = win_width;
+	}
+	else  {
+		x1 += step; x2 += step;
+	}
 }
+
+void Role::move_left() {
+	if (dir == 1) dir = 0;
+	if (x1 - step < 0) {
+		x2 = 0 + (x2 - x1);
+		x1 = 0;
+	}
+	else {
+		x1 -= step; x2 -= step;
+	}
+}
+
+//需要物理引擎
+void Role::jump_up() {
+	if (y1 - jump < 0) {
+		y2 = 0 + (y2 - y1);
+		y1 = 0;
+	}
+	else {
+		y1 -= jump;
+		y2 -= jump;
+	}
+}
+
+void Role::squat_down() {
+	y1 += (y2 - y1) / 2;
+}
+
+//按下某鍵作出防禦狀態，並且作出防禦動作
+void Role::defense_enermy() {
+	if (rolestatus != 1)
+		rolestatus = 1;
+	//呼叫防禦動作特效
+	//防禦結束要變回idle。
+}
+
+//按下某鍵作出普通攻擊姿勢
+void Role::attack_enermy(Role* Enemy) {
+	if (rolestatus != 2)
+		rolestatus = 2;
+	//呼叫攻擊特效
+	//攻擊結束要變回idle。
+}
+//Release ability once the role has full Energy
+void Role::release_ability() {
+	if (rolestatus != 3) {
+		rolestatus = 3;
+	}
+	if (/*放技能成功*/) {
+		reset_Energy();
+	}
+	//放技能狀態結束後變回idle。
+}
+
+
--- BoxingGame/Role.h
+++ BoxingGame/Role.h
@@ -1,41 +1,52 @@
 #pragma once
+#pragma once
 #ifndef ROLE_H_INCLUDED
 #define ROLE_H_INCLUDED
-#include "Object.h"
-#include "Global.h"
 #define MAX_ENERGY 100
+#include <string>
+
+using namespace std;
 
 const int step = 27; // The distance that the role walks at one time. 
-const int jump = 300; // The height that the role jumps 
+const int jump = 200; // The height that the role jumps 
 
 
-class Role : public Object
+class Role
 {
 public:
-	Role(int, int, int, string);
+	Role(int _hp, int _atk, int _def, int _dir, string _name);
 	virtual ~Role();
-	//overide the virtual funciton Object::Draw()
-	void Draw();
-	void Load_Attack();
-	void Load_Move();
-	void Load_Ult();
-	//Detect if it move to the edge of window.
-	bool move();
+	//取得生命值，攻擊力，怒氣值
+	int getATK();
+	int getHP();
+	int getEnergy();
+	int getDefense();
+
+	//movement of the role:
+	void move_right();
+	void move_left();
+	void jump_up();
+	void squat_down(); 
 
 	//Detect if the HP is 0 after substraction. 
-	bool substract_HP(int); 
+	bool substractHP(int);
 
 	//If the attack is hit successfully, the energy will increase. If the energy is 100, the role can release the ultimate ability. 
 	//Detect if the energy is full to 100.
-	bool increase_Energy(); 
+	bool increase_Energy();
 	void reset_Energy();
-protected:
-	int HealthPoint, Defence, AttackPower, Energy; //角色數值 
-	int x1, y1, x2, y2, dir; //Position and direction of role
-	string Name;
-
+	//Movement, x_move is to move on the x_axis, y_move is to move on the y_axis. 
+	
+	//攻擊對手、防禦對手、放技能
+	void attack_enermy(Role *Enemy);
+	void defense_enermy();
+	void release_ability();
+	
 private:
-
+	int HealthPoint, AttackPower, Energy, Defense; //角色數值 
+	int x1, x2, y1, y2, dir; //Position and direction of role, direction 0 means left, direction 1 is right;
+	string Name;
+	int rolestatus; //0代表idle、1代表防禦狀態、2代表攻擊狀態、3放技能狀態
 };
 
 #endif
